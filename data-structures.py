fecce10ec61af6bfa1fc91b95fc13e4ac558ff94
gunjan3310 <yadavgunjan3310@live.com>
2020-08-09 21:01:51 +0545

    list comprehension and sets with their usage

--- data-structures.py
+++ data-structures.py
@@ -44,3 +44,29 @@ print("The key value dictionary in total", myDict)
 
 #Dictionary specific features
 print("myDict has 'data_structure' key and the associated value upon call is: ",myDict["data_structure"])
+
+#list comprehensions examples
+cordinates = [(x,y) for x in range(6) for y in range(0,6) if x==y]
+print("Cordinates for line y=x",cordinates)
+
+matrix = [
+[1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 10, 11, 12],
+]
+transposed = [[row[i] for row in matrix] for i in range(4)] #nested list comprehension it's difficult withour famailirity
+print(transposed)
+
+#sets
+print("Sets and their usage")
+a = set('abcdefghijklmnopqrstuvwxyz')
+b = set('Gunjan Yadav')
+
+print("letters in a or b or both",a|b)
+print("letters in a but not in b",a-b)
+print("letters in both a and b",(a&b))
+print("letter in a or b but not in both",a^b)
+#list comprehension in set
+
+setcomprehension = {x for x in range(6)}
+print("Set comprehended ", setcomprehension)
