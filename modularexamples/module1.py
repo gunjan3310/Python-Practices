b18f03c3df8300c12710af1d182347de9107157b
gunjan3310 <yadavgunjan3310@live.com>
2020-08-11 14:16:35 +0545

    a module that can be imported to use its content of functionality

---
+++ modularexamples/module1.py
@@ -0,0 +1,23 @@
+# Fibonacci numbers module
+
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n):   # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+if __name__ == "__main__": #when this module is executed this main will be called
+    import sys #imported from standard python packages
+    print("Ok the module is run")
+    for n in sys.argv[1:]:
+        fib(int(n))
+
